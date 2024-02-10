07a3b87a95d1f0ed5e50173c62bfb94a9ad1cc15
InquestGeronimo <rcosta84@gmail.com>
2024-02-10 11:27:12 -0500

    refactored code

--- tllm/train.py
+++ tllm/train.py
@@ -8,6 +8,15 @@ import transformers
 from datetime import datetime
 from peft import PeftModel
 
+from accelerate import FullyShardedDataParallelPlugin, Accelerator
+from torch.distributed.fsdp.fully_sharded_data_parallel import FullOptimStateDictConfig, FullStateDictConfig
+
+fsdp_plugin = FullyShardedDataParallelPlugin(
+    state_dict_config=FullStateDictConfig(offload_to_cpu=True, rank0_only=False),
+    optim_state_dict_config=FullOptimStateDictConfig(offload_to_cpu=True, rank0_only=False),
+)
+
+accelerator = Accelerator(fsdp_plugin=fsdp_plugin)
 
 class LLMTrainer:
     """
@@ -15,55 +24,89 @@ class LLMTrainer:
 
     Args:
         base_model_id (str): The identifier for the base Llama 2 7B model from Hugging Face.
-        output_dir (str): The directory where model checkpoints and logs will be saved.
         train_dataset_path (str): The stub of the training dataset on HF Hub.
         eval_dataset_path (str): The stub of the evaluation dataset on HF Hub.
     """
 
     def __init__(
-        self, base_model_id, output_dir, train_dataset_path, eval_dataset_path=None
+        self, model_id, dataset_id
     ):
-        self.base_model_id = base_model_id
-        self.output_dir = output_dir
-        self.train_dataset_path = train_dataset_path
-        self.eval_dataset_path = eval_dataset_path
-
-    def load_datasets(self):
-        """
-        Load training and evaluation datasets.
-        """
-        train_dataset = load_dataset(path=self.train_dataset_path, split="train"
-        )
-        eval_dataset = load_dataset(path=self.eval_dataset_path, split="eval"
-        )
-        return train_dataset, eval_dataset
-
-    def prepare_model(self):
-        """
-        Prepare the model for fine-tuning with QLoRA.
-        """
-        bnb_config = BitsAndBytesConfig(
+        self.model_id = model_id
+        self.dataset_id = dataset_id
+        
+        self.bnb_config = BitsAndBytesConfig(
             load_in_4bit=True,
             bnb_4bit_use_double_quant=True,
             bnb_4bit_quant_type="nf4",
             bnb_4bit_compute_dtype=torch.bfloat16,
         )
-        base_model = AutoModelForCausalLM.from_pretrained(
-            self.base_model_id, quantization_config=bnb_config
+
+    def load_datasets(self):
+        """
+        Load training and evaluation datasets.
+        """
+        train_dataset = load_dataset(path=self.dataset_id, split="train"
         )
+        eval_dataset = load_dataset(path=self.dataset_id, split="validation"
+        )
+        return train_dataset, eval_dataset
+
+    def load_model_and_tokenizer(self):
+        """
+        Prepare the model for fine-tuning with QLoRA.
+        """
+
+        model = AutoModelForCausalLM.from_pretrained(
+            self.model_id, quantization_config=self.bnb_config, trust_remote_code=True,
+        )
+
         tokenizer = AutoTokenizer.from_pretrained(
-            self.base_model_id,
+            self.model_id,
             padding_side="left",
-            add_eos_token=True,
-            add_bos_token=True,
+            add_eos_token=True,  
+            add_bos_token=True,  
         )
+        
+        return model, tokenizer
+    
+    def create_prompts_from_datasets(self, tokenizer, train_data, eval_data):
+        
         tokenizer.pad_token = tokenizer.eos_token
 
-        model = prepare_model_for_kbit_training(base_model)
+        def tokenize(prompt):
+            result = tokenizer(
+                prompt,
+                truncation=True,
+                max_length=340,
+                padding="max_length",
+            )
+            result["labels"] = result["input_ids"].copy()
+            return result
+        
+        def generate_and_tokenize_prompt(data_point):
+            full_prompt =f"""Given a target sentence construct the underlying meaning representation of the input sentence as a single function with attributes and attribute values.
+        This function should describe the target string accurately and the function must be one of the following ['inform', 'request', 'give_opinion', 'confirm', 'verify_attribute', 'suggest', 'request_explanation', 'recommend', 'request_attribute'].
+        The attributes must be one of the following: ['name', 'exp_release_date', 'release_year', 'developer', 'esrb', 'rating', 'genres', 'player_perspective', 'has_multiplayer', 'platforms', 'available_on_steam', 'has_linux_release', 'has_mac_release', 'specifier']
+
+        ### Target sentence:
+        {data_point["target"]}
+
+        ### Meaning representation:
+        {data_point["meaning_representation"]}
+        """
+            return tokenize(full_prompt)
+        
+        tokenized_train_dataset = train_data.map(generate_and_tokenize_prompt)
+        tokenized_eval_dataset = eval_data.map(generate_and_tokenize_prompt)
+
+        return tokenized_train_dataset, tokenized_eval_dataset
+
+    def configure_lora(self, model):
+        model = prepare_model_for_kbit_training(model)
 
         config = LoraConfig(
-            r=32,
-            lora_alpha=64,
+            r=8,
+            lora_alpha=16,
             target_modules=[
                 "q_proj",
                 "k_proj",
@@ -80,9 +123,10 @@ class LLMTrainer:
         )
 
         model = get_peft_model(model, config)
-        return model, tokenizer
+        model = accelerator.prepare_model(model)
+        return model
 
-    def setup_training(self, model, tokenizer, train_dataset, eval_dataset=None):
+    def setup_training(self, model, tokenizer, train_dataset, eval_dataset):
         """
         Set up the training configuration using Transformers Trainer.
         """
@@ -90,7 +134,7 @@ class LLMTrainer:
             model.is_parallelizable = True
             model.model_parallel = True
 
-        project = "journal-finetune"
+        project = "model-finetune"
         run_name = "llama2-7b-" + project
 
         tokenizer.pad_token = tokenizer.eos_token
@@ -100,7 +144,7 @@ class LLMTrainer:
             train_dataset=train_dataset,
             eval_dataset=eval_dataset,
             args=TrainingArguments(
-                output_dir=self.output_dir,
+                output_dir="./output",
                 warmup_steps=1,
                 per_device_train_batch_size=2,
                 gradient_accumulation_steps=1,
@@ -116,6 +160,7 @@ class LLMTrainer:
                 eval_steps=50,
                 do_eval=True,
                 report_to="wandb",
+                remove_unused_columns=True,
                 run_name=f"{run_name}-{datetime.now().strftime('%Y-%m-%d-%H-%M')}",
             ),
             data_collator=DataCollatorForLanguageModeling(tokenizer, mlm=False),
@@ -130,10 +175,11 @@ class LLMTrainer:
         return trainer
     
     def train_model(self, trainer):
-        """
-        Train the fine-tuned model.
-        """
-        trainer.train()
+            """
+            Run the fine-tuning training on LLM.
+            """
+            trainer.train()
+
 
     def load_finetuned_model(self, checkpoint_path):
         """
