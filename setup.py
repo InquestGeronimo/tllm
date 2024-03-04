ff71b95ddc805e6e4f8662afbb0450bc71f9137d
InquestGeronimo <rcosta84@gmail.com>
2024-03-03 21:08:39 -0500

    edit version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     LONG_DESCRIPTION = "\n" + fh.read()
 
 NAME = "tllm"
-VERSION = "0.0.0.4"
+VERSION = "0.0.4"
 AUTHOR = "InquestGeronimo"
 EMAIL = "rcostanl@gmail.com"
 LD_CONTENT_TYPE = "text/markdown"
